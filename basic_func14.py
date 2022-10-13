535426ee088c9c1fcbe0bc809fca23aa56aca4f2
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-13 23:32:56 +0500

    basic_func14.py

--- basic_func14.py
+++ basic_func14.py
@@ -1,3 +1,10 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Returns the floor division of a and b.
+from operator import floordiv
+
+
+def main(a,b):
+    return floordiv(a,b)
+
+print(main(10,4))
