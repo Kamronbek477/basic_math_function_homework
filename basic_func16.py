9a844de8469c269c5c552c91d3a18573c2cabd27
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-13 23:43:20 +0500

    basic_func16.py

--- basic_func16.py
+++ basic_func16.py
@@ -1,3 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Returns the sum of a and b.
+from operator import add
+
+
+def main(a,b):
+    return add(a,b)
+print(main(5,21))
