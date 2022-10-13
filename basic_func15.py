e61ab917297b62a3ce62ac8cc7178fcd755599af
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-13 23:38:52 +0500

    basic_func15.py

--- basic_func15.py
+++ basic_func15.py
@@ -1,3 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the modulus of a and b.
+from operator import mod
+
+
+def main(a,b):
+    return mod(a,b)
+print(main(29,10))
