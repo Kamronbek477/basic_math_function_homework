62b6f2b07b88d83b90dfefba8e61054bcf5b537b
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-14 09:29:49 +0500

    basic_func18.py

--- basic_func18.py
+++ basic_func18.py
@@ -2,3 +2,10 @@
 # Create function arguments a.
 # Assign the value pi to the variable "a" and return.
 # Round the result to 2 decimal places.
+from cmath import pi
+
+
+def main(a,b):
+    
+    return round(a,b)
+print(main(pi,2))
