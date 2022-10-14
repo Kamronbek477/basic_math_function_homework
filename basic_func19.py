110f31cd7e9262b43281afd78eb8e756ec125ffa
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-14 09:27:20 +0500

    basic_func19.py

--- basic_func19.py
+++ basic_func19.py
@@ -1,3 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the absolute value of the difference between a and b.
+from operator import sub
+
+
+def main(a,b):
+    return abs(sub(a,b))
+print(main(4,9))
