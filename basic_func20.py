cc9b63d2f1e750541d55cda8f4104fdfc78e01fc
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-10-14 09:22:58 +0500

    basic_func20.py

--- basic_func20.py
+++ basic_func20.py
@@ -1,3 +1,9 @@
 # Create a function called main.
 # Create function arguments a and b.
 # Return the multiplication of a and b.
+from operator import mul
+
+
+def main(a,b):
+    return mul(a,b)
+print(main(4,5))
